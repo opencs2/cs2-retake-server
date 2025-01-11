e20cd68c5bde10da90938570de635f0d9cc8c684
krbtgt <144264613+goldenkirbi@users.noreply.github.com>
2025-01-11 12:30:20 +0000

    agones passthrough

--- bullseye/etc/post.sh
+++ bullseye/etc/post.sh
@@ -2,4 +2,14 @@
 
 # POST HOOK
 #  Make your customisation here
-echo "post-hook: noop"
+echo "post-hook: attempting to configure portPolicy: passthrough"
+
+# TODO: check if the returned gameserver spec contains portPolity setting. If so, only attempt to set CS2_PORT if port policy is set to passthrough.
+
+PASSTHROUGH_PORT=$(curl -s -H "Content-Type: application/json" -X GET http://localhost:${AGONES_SDK_HTTP_PORT}/gameserver | jq '.status.ports[] | select(.name == "default-tcp").port')
+
+if [ ! -z $PASSTHROUGH_PORT]; then
+  echo "setting CS2_PORT to ${PASSTHROUGH_PORT}"
+  CS2_PORT=$PASSTHROUGH_PORT
+fi;
+
--- bullseye/etc/pre.sh
+++ bullseye/etc/pre.sh
@@ -2,4 +2,14 @@
 
 # PRE HOOK
 #  Make your customisation here
-echo "pre-hook: noop"
+echo "pre-hook: attempting to configure portPolicy: passthrough"
+
+# TODO: check if the returned gameserver spec contains portPolity setting. If so, only attempt to set CS2_PORT if port policy is set to passthrough.
+
+PASSTHROUGH_PORT=$(curl -s -H "Content-Type: application/json" -X GET http://localhost:${AGONES_SDK_HTTP_PORT}/gameserver | jq '.status.ports[] | select(.name == "default-tcp").port')
+
+if [ ! -z $PASSTHROUGH_PORT ]; then
+  echo "pre=hook: setting CS2_PORT to ${PASSTHROUGH_PORT}"
+  CS2_PORT=$PASSTHROUGH_PORT
+fi;
+
